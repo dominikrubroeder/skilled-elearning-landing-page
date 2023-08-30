bee2fa56148a88a8eddd72177b0ed9f5c125ff6b
Dominik Rubröder <dominik.rubroeder@icloud.com>
2023-08-30 15:29:24 +0200

    Optimize Hero component for all viewports

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,52 +1,39 @@
-import Image from 'next/image';
 import React from 'react';
-import Button, { ButtonVariants } from './Button';
+import Button from './Button';
+import Image from "next/image";
 
 const Hero: React.FC = () => {
   return (
-    <section>
-      <div className="sm:flex sm:items-center sm:justify-between max-w-screen-lg m-auto">
-        <div className="flex-1 grid gap-8 max-w-sm">
-          <div className="grid gap-4">
-            <h1 className="text-5xl font-bold">
-              Maximize skill,
-              <br />
-              minimize budget
-            </h1>
-            <p className="text-app-gray">
-              Our modern courses across a range of in-demand skills will give
-              you the knowledge you need to live the life you want.
-            </p>
-          </div>
-
-          <Button variant={ButtonVariants.containedGradientOrangePink}>
-            Get started
-          </Button>
+    <section className="relative max-w-contained px-4 xl:px-0 mx-auto w-full -mb-8 md:mb-0 md:flex md:items-center lg:h-[65vh]">
+      <div className="grid gap-8 max-w-sm md:mb-40">
+        <div className="grid gap-4">
+          <h1 className="text-5xl font-bold">
+            Maximize skill,
+            <br />
+            minimize budget
+          </h1>
+          <p className="text-app-gray">
+            Our modern courses across a range of in-demand skills will give
+            you the knowledge you need to live the life you want.
+          </p>
         </div>
 
-        <div className="flex-1">
-          <div className="hidden sm:block">
-            <Image
-              src="/images/image-hero-desktop.png"
-              alt="Hero image"
-              width={1046}
-              height={938}
-              layout="responsive"
-              priority
-            />
-          </div>
+        <Button variant="Gradient-Orange-Pink">
+          Get started
+        </Button>
+      </div>
 
-          <div className="sm:hidden">
-            <Image
-              src="/images/image-hero-mobile.png"
-              alt="Hero image"
-              width={435}
-              height={409}
-              layout="responsive"
-              priority
-            />
-          </div>
-        </div>
+      <div className="hidden absolute right-0 top-0 md:block md:w-[40rem] md:h-[40rem] md:translate-x-[38%] md:-translate-y-[30%] lg:w-[58rem] lg:h-[58rem] lg:translate-x-[48%] lg:-translate-y-[26%]">
+        <Image width="2092"
+               height="1876"
+               src="/images/image-hero-desktop@2x.png"
+               sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+               alt="Hero"
+               fill />
+      </div>
+
+      <div className="relative h-[20rem] w-[20rem] mx-auto mt-8 md:hidden">
+        <Image width={870} height={818} src="/images/image-hero-mobile@2x.png" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" alt="Hero mobile" fill />
       </div>
     </section>
   );
