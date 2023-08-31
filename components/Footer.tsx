9d8b66d4e7e298c3b6eb0f2ca803f6b78f2fb13d
Dominik Rubröder <dominik.rubroeder@icloud.com>
2023-08-31 11:57:53 +0200

    Separate Footer in it own component

---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+    return <footer className="bg-app-blue-dark text-center px-4 pb-4 text-app-gray text-xs">
+        <p className="max-w-[60.375rem] mx-auto">
+            Frontend challenge by
+            <a
+                href="https://www.frontendmentor.io/challenges/skilled-elearning-landing-page-S1ObDrZ8q"
+                target="_blank"
+                rel="noreferrer"
+                className="pl-1"
+            >
+                frontendmentor.io
+            </a>
+            , done by Dominik Rubröder
+        </p>
+    </footer>
+}
