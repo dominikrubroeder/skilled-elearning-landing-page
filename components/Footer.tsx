d6fac1e177808efd8bbbf74e3882b984ddb59ee9
Dominik Rubröder <dominik.rubroeder@icloud.com>
2023-08-31 12:07:52 +0200

    Add GitHub Link signature to Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,25 @@
 export default function Footer() {
-    return <footer className="bg-app-blue-dark text-center px-4 pb-4 text-app-gray text-xs">
-        <p className="max-w-[60.375rem] mx-auto">
-            Frontend challenge by
-            <a
-                href="https://www.frontendmentor.io/challenges/skilled-elearning-landing-page-S1ObDrZ8q"
-                target="_blank"
-                rel="noreferrer"
-                className="pl-1"
-            >
-                frontendmentor.io
-            </a>
-            , done by Dominik Rubröder
-        </p>
+  return (
+    <footer className="bg-app-blue-dark px-4 pb-4 text-center text-xs text-app-gray">
+      <p className="mx-auto max-w-[60.375rem]">
+        Frontend challenge by
+        <a
+          href="https://www.frontendmentor.io/challenges/skilled-elearning-landing-page-S1ObDrZ8q"
+          target="_blank"
+          rel="noreferrer"
+          className="pl-1"
+        >
+          frontendmentor.io
+        </a>
+        , done by&nbsp;
+        <a
+          href="https://github.com/dominikrubroeder"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Dominik Rubröder
+        </a>
+      </p>
     </footer>
+  );
 }
