4bfe4d9417c92054ca70ab3a0cd80e82f2aab6fd
Dominik Rubröder <dominik.rubroeder@icloud.com>
2023-08-31 12:00:40 +0200

    Fix Head meta information warning in _document.tsx file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Skilled elearning landing page | frontendmentor.io</title>
+        <meta
+          name="description"
+          content="Skilled elearning landing page | Frontend challenge by frontendmentor.io | Developed by Dominik Rubröder"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,15 +4,7 @@ import React from "react";
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <title>Skilled elearning landing page | frontendmentor.io</title>
-        <meta
-          name="description"
-          content="Skilled elearning landing page | Frontend challenge by frontendmentor.io | Developed by Dominik Rubröder"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
+      <Head />
       <body className="bg-gradient-white-blue bg-gradient-to-b">
         <Main />
         <NextScript />
