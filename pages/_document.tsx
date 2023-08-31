4f02c961ec11bc7683462f9e9cb59bcf45fd33bc
Dominik Rubröder <dominik.rubroeder@icloud.com>
2023-08-31 11:55:37 +0200

    Add page's meta information to _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,19 @@
-// pages/_document.js
 import { Head, Html, Main, NextScript } from "next/document";
+import React from "react";
 
 export default function Document() {
   return (
     <Html>
-      <Head />
-      <body className="bg-gradient-white-blue overflow-x-hidden bg-gradient-to-b">
+      <Head>
+        <title>Skilled elearning landing page | frontendmentor.io</title>
+        <meta
+          name="description"
+          content="Skilled elearning landing page | Frontend challenge by frontendmentor.io | Developed by Dominik Rubröder"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <body className="bg-gradient-white-blue bg-gradient-to-b">
         <Main />
         <NextScript />
       </body>
